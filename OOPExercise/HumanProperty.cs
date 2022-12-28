f536f5908fbfc9806a1cada242a29c0d6b68c30d
khas <khas@KHASPC1027>
2022-12-28 15:03:32 +0300

    Add project files.

---
+++ OOPExercise/HumanProperty.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExercise
+{
+    internal class HumanProperty
+    {
+        
+        public string Isim { get; set; } = "Gökçe Betül";
+        public string Soyisim { get; set; } = "ÇAKMAK";
+        public int Boy { get; set; } = 164;
+        public int Kilo { get; set; } = 48;
+        public int Yas { get; set; } = 26;
+
+
+        public void Tanis()
+        {
+            Console.WriteLine("Ben {0} {1}, {2}cm boyundayım,{3} kiloyum, Yaşım {4}.", Isim, Soyisim, Boy, Kilo, Yas);
+
+        }
+    }
+
+    
+
+
+
+    
+
+}
---
+++ OOPExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using OOPExercise;
+
+internal class Program
+{
+    // İnsan isimli bir sınıf içinde ad, soyad, boy, kilo, yaş alanlarını tanımlayıp, bir kurucu -constructor- metod içinde bu alanlar için değer atamalarını yapacağız. Son olarak da "Tanis" isimli metodla girilen bilgileri kullanıcıya belli, güzel, açıklayıcı bir şekilde görünteleyen bir programcık yazınız
+    //Örnek : Ben Gökçe Betül ÇAKMAK...Boyum 164cm, Kilom 48kg, Yaşım 26'dır.
+
+    
+    private static void Main(string[] args)
+    {
+       HumanProperty humanProperty = new HumanProperty();
+       humanProperty.Tanis();
+       Console.ReadKey();
+    }
+}
